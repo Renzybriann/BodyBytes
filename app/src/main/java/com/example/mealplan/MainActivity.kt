5f9f3cb6b93b6c042d4795dbc76f83aaa937df40
kimatzu <mattzkie07@gmail.com>
2023-05-23 22:23:53 +0800

    SplashScreen

--- app/src/main/java/com/example/mealplan/MainActivity.kt
+++ app/src/main/java/com/example/mealplan/MainActivity.kt
@@ -4,21 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import java.lang.Thread.sleep
 
 class  MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
 
-
-
-        val buttonClick = findViewById<Button>(R.id.PressToContinue)
-        buttonClick.setOnClickListener {
-            val intent = Intent(this, Login::class.java)
-            startActivity(intent)
+        val thread = Thread {
+            try {
+                sleep(2000)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                val newIntent = Intent(this@MainActivity, Login::class.java)
+                startActivity(newIntent)
+                finish()
+            }
         }
-
+        thread.start()
 
        // val buttonClick1 = findViewById<Button>(R.id.button7)
        // buttonClick1.setOnClickListener {
